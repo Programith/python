442062d95e1d0266aed40e0e827ed1a513275185
Programith <142921610+Programith@users.noreply.github.com>
2023-08-30 23:07:24 +0200

    Add print

---
+++ print.py
@@ -0,0 +1,13 @@
+#
+# @Programith
+#
+
+name = 'Anna'
+age = 24
+
+print(name + ' : ' + str(age))
+print(name, ':', age)
+print(name, age, sep=' : ')
+print(f'{name} : {age}')
+print('%s : %s' % (name, age))
+print('{} : {}'.format(name, age))
